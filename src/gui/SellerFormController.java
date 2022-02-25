7119cb767c5cce00467658e6ed003d1207de67b0
Ruan Rodrigues <rupriwell123@gmail.com>
2022-02-25 16:11:51 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,12 @@
+
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -36,60 +39,60 @@ import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
-
+	
 	private Seller entity;
-
+	
 	private SellerService service;
-
+	
 	private DepartmentService departmentService;
-
+	
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
-
+	
 	@FXML
 	private TextField txtId;
-
+	
 	@FXML
 	private TextField txtName;
-
+	
 	@FXML
 	private TextField txtEmail;
-
+	
 	@FXML
 	private DatePicker dpBirthDate;
-
+	
 	@FXML
 	private TextField txtBaseSalary;
-
+	
 	@FXML
 	private ComboBox<Department> comboBoxDepartment;
-
+	
 	@FXML
 	private Label labelErrorName;
-
+	
 	@FXML
 	private Label labelErrorEmail;
-
+	
 	@FXML
 	private Label labelErrorBirthDate;
-
+	
 	@FXML
 	private Label labelErrorBaseSalary;
-
+	
 	@FXML
 	private Button btSave;
-
+	
 	@FXML
 	private Button btCancel;
-
+	
 	private ObservableList<Department> obsList;
 
 	public void setSeller(Seller entity) {
 		this.entity = entity;
 	}
 
-	public void setServices(SellerService service, DepartmentService departmentservice) {
+	public void setServices(SellerService service, DepartmentService departmentService) {
 		this.service = service;
-		this.departmentService = departmentservice;
+		this.departmentService = departmentService;
 	}
 
 	public void subscribeDataChangeListener(DataChangeListener listener) {
@@ -103,10 +106,9 @@ public class SellerFormController implements Initializable {
 		}
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
-
 		}
 		try {
-			entity = getFormDate();
+			entity = getFormData();
 			service.saveUpdate(entity);
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
@@ -123,22 +125,39 @@ public class SellerFormController implements Initializable {
 		}
 	}
 
-	private Seller getFormDate() {
+	private Seller getFormData() {
 		Seller obj = new Seller();
-
+		
 		ValidationException exception = new ValidationException("Validation error");
-
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
-
+		
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBitchDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		obj.setDepartment(comboBoxDepartment.getValue());
+
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
-
 		return obj;
 	}
 
@@ -148,7 +167,7 @@ public class SellerFormController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL aurl, ResourceBundle rb) {
+	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
 
@@ -158,8 +177,8 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
-		
-		initializeComboBoxDepartment(); 
+
+		initializeComboBoxDepartment();
 	}
 
 	public void updateFormData() {
@@ -174,18 +193,16 @@ public class SellerFormController implements Initializable {
 		if (entity.getBitchDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBitchDate().toInstant(), ZoneId.systemDefault()));
 		}
-		if(entity.getDepartment() == null) {
+		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
-		}
-		else {
+		} else {
 			comboBoxDepartment.setValue(entity.getDepartment());
 		}
-		comboBoxDepartment.setValue(entity.getDepartment());
 	}
 
 	public void loadAssociatedObjects() {
 		if (departmentService == null) {
-			throw new IllegalStateException("Department was null");
+			throw new IllegalStateException("DepartmentService was null");
 		}
 		List<Department> list = departmentService.findAll();
 		obsList = FXCollections.observableArrayList(list);
@@ -198,6 +215,10 @@ public class SellerFormController implements Initializable {
 		if (fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
 		}
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 	}
 
 	private void initializeComboBoxDepartment() {
